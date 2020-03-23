1ac2793d126b4ce4b8423ead8e3283b63b5d3160
unknown <freddyhenao28.fh2gmail.com>
2020-03-22 21:15:02 -0500

    se agrega  Algoritmo2 en la rama alg2a6

---
+++ Algoritmo2.js
@@ -0,0 +1,21 @@
+/*
+2) Algoritmo que lea dos números, calculando y escribiendo el valor de
+ su suma, resta, producto y división.  */
+let A=0;
+let B=0;
+let SUMA=0;
+let RESTA =0;
+let PRODUCTO=0;
+let DIVISION=0;
+
+
+alert("Algoritmo 2, Algoritmo que lea dos números, calculando y escribiendo el valor de su suma, resta, producto y división.");
+
+A=parseInt(prompt("Ingrese A"));
+B=parseInt(prompt("Ingrese B"));
+
+SUMA = A+B;
+RESTA = A-B;
+PRODUCTO = A*B;
+DIVISION = A/B;
+alert("RESULRADOS  SUMA = " + SUMA + " RESTA =" + RESTA + " PRODUCTO = " + PRODUCTO + "DIVISION =" + PRODUCTO);
