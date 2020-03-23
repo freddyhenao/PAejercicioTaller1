894e906f2d87219c1ae57d4375062c548bb5d823
unknown <freddyhenao28.fh2gmail.com>
2020-03-22 23:07:56 -0500

    se agrega  Algoritmo5  en la rama alg2a6

---
+++ Algoritmo5.js
@@ -0,0 +1,28 @@
+/*
+5) Diseñar un algoritmo que pida por teclado tres números;
+ si el primero es negativo, debe imprimir el producto de los 
+tres y si no lo es, imprimirá la suma. 
+*/
+let A=0;
+let B=0;
+let C=0;
+let PRODUCTO=0;
+let SUMA=0;
+
+
+
+ A =parseInt(prompt("ingrese el primer numero"));    
+ B =parseInt(prompt("ingrese el segundo numero"));
+ C =parseInt(prompt("ingrese el tercer numero"));
+
+if (A<0) {
+     PRODUCTO = A*B*C;
+         alert("EL PRODUCTO DE LOS 3 NUMEROS ES  : "+PRODUCTO);
+} else {
+        SUMA = A+B+C
+         alert("LA SUMA DE LOS 3 NUMEROS ES  :  "+SUMA);   
+}
+      
+ 
+
+
