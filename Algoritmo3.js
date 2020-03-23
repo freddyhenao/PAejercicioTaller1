7eee2fc3f8f14243016e23f16efabc60bb672cdf
unknown <freddyhenao28.fh2gmail.com>
2020-03-22 21:52:11 -0500

    se agrega  Algoritmo3 en la rama alg2a6

---
+++ Algoritmo3.js
@@ -0,0 +1,19 @@
+/* 
+3) Algoritmo que lea dos números y nos diga cual de ellos es mayor 
+o bien si son iguales (recuerda usar la estructura condicional SI)  */
+let A=0;
+let B=0;
+let SUMA=0;
+let RESTA =0;
+let PRODUCTO=0;
+let DIVISION=0;
+
+
+ A =parseInt(prompt("ingrese el primer numero"));    ;
+ B =parseInt(prompt("ingrese el segundo numero"));
+if(A===B)
+    alert("son iguales");
+if(A>B)
+    alert("el numero mayor es : "+A);
+if(B>A)
+    alert("el numero mayor es : "+B);
