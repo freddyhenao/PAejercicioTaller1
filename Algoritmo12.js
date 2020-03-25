2cb2d8d946472d7f3224acd2c3c1ecc34cc7451c
unknown <freddyhenao28.fh2gmail.com>
2020-03-25 10:11:56 -0500

    se agrega algorito 12 a rama alg7a12

---
+++ Algoritmo12.js
@@ -0,0 +1,43 @@
+    /**
+     12) Desarrollar un algoritmo que nos calcule el cuadrado de los 9 
+         primeros números naturales (recuerda la estructura desde-hasta) ,
+        
+         calcula la raiz cuadrada de un numero 
+         var mostrar = prompt("introduce un numero"); 
+        var resultado = Math.sqrt(mostrar);
+        console.log(resultado) 
+    
+
+        ciclo 
+
+        class whileLoopDemo
+{
+    public static void main(String args[])
+    {
+        int x = 1;
+ 
+        // Salir cuando x llega a ser mayor que 4
+        while (x <= 4)
+        {
+            System.out.println("Valor de x: " + x);
+ 
+            //incrementa el valor de x para la siguiente iteración
+            x++;
+
+
+     */
+
+let x=1;
+let resultado=0;
+
+while (x <= 9)
+{
+    resultado = x*x;
+ alert( "El Cuadrado x : "+ resultado);
+    x++;
+}
+
+
+ 
+ 
+    
